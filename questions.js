d44ac055e36a3713981e8da7e1593d24783cce06
Moin Virani <moin.virani@gmail.com>
2014-01-07 20:09:14 -0500

    added email handlebars to show email of current user asking question

--- questions.js
+++ questions.js
@@ -1,6 +1,12 @@
 // This is deliberately a global variable
 Questions = new Meteor.Collection('Questions');
 
+var getCurrentEmail = function () {
+  return Meteor.user() &&
+  Meteor.user().emails &&
+  Meteor.user().emails[0].address;
+};
+
 if (Meteor.isClient) {
   Template.questions.allQuestions = function () {
     return Questions.find();
@@ -9,7 +15,12 @@ if (Meteor.isClient) {
   Template.questions.events({
     "click #questionAsk": function (evnt, templ) {
       var question = templ.find("#questionText").value;
-      Questions.insert({question: question, score: 1});
+      Questions.insert({
+        question: question,
+        score: 1,
+        email: getCurrentEmail(),
+        votes: [Meteor.userId()]
+      });
     }
   });
 
